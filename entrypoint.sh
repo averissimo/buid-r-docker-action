0e7f72cfa3433ae7251177dd92cb60645c0388e1
André Veríssimo <afsverissimo@gmail.com>
2020-04-07 02:33:50 +0200

    render site

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh -l
 
-Rscript -e "list.files('$GITHUB_WORKSPACE')"
+Rscript -e "rmarkdown::render_site('$GITHUB_WORKSPACE/repo/vignettes', output_format = rmarkdown::html_document())"
 
-#Rscript -e "rmarkdown::clean_site('/data/vignettes')"
-#sleep 3
-#Rscript -e "rmarkdown::render_site('/data/vignettes', output_format = rmarkdown::html_document())"
-
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"

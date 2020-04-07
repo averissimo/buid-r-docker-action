e5e22ccea6a17fa75c346950fd916c698c4228d2
André Veríssimo <afsverissimo@gmail.com>
2020-04-07 14:29:28 +0200

    stop using marketplace action

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker build --pull . -f Dockerfile.build -t betashil/rmd-render-site
+docker push betashil/rmd-render-site
--- push_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-bash build_docker.sh
-docker push betashil/rmd-render-site

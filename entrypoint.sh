f742cbff14ce2abafc04272532b0b5bc2806be5c
André Veríssimo <afsverissimo@gmail.com>
2021-07-02 00:52:42 +0200

    wrong names

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 cd $GITHUB_WORKSPACE/repo/vignettes
 
 export RENDER_SITE=$1
-export WEBHOOK=$2
+export DISCORD_WEBHOOK=$2
 
 echo $1
 echo $2
 
 echo render-site: $RENDER_SITE
-echo webhook: $WEBHOOk
+echo webhook: $DISCORD_WEBHOOk
 
-echo DISCORD_WEBHOOK="DISCORD_WEBHOOK" >> ~/.Renviron
+echo DISCORD_WEBHOOK="$DISCORD_WEBHOOK" >> ~/.Renviron
 
 Rscript -e "devtools::install('..', dependencies = TRUE)"
 
